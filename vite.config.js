4c9e309c19f6ce5ddb93612e3f87915a256564bd
ubercatz <155195884+ubercatz@users.noreply.github.com>
2026-08-11 02:56:36 -0700

    this is a pain in the ass

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.NODE_ENV': JSON.stringify('production')
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env': JSON.stringify({ NODE_ENV: 'production' }),
+    'process': JSON.stringify({ env: { NODE_ENV: 'production' } })
   },
   build: {
+    // ... keep all your existing build and rollupOptions settings down here!
     outDir: 'dist',
     emptyOutDir: true,
     sourcemap: true,
